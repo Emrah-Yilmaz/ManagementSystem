db0c872b6c58a0d7a139775a3003dd8a385711bd
Emrah Yilmaz <emrahyilmaz57@outlook.com.tr>
2024-03-29 00:05:46 +0300

    feat: updated request model

--- Services/ManagementSystem.WebApi/MappingProfile/WorkTaskMappingProfile.cs
+++ Services/ManagementSystem.WebApi/MappingProfile/WorkTaskMappingProfile.cs
@@ -1,5 +1,4 @@
 ﻿using ManagementSystem.Application.Features.Queries.WorkTask;
-using ManagementSystem.WebApi.Models.WorkTask.Request;
 using ManagementSystem.WebApi.Models.WorkTask.Response;
 
 namespace ManagementSystem.WebApi.MappingProfile
@@ -9,7 +8,6 @@ namespace ManagementSystem.WebApi.MappingProfile
         public WorkTaskMappingProfile()
         {
             CreateMap<WorkTasksDto, WorkTasksResponse>();
-            CreateMap<WorkTaskRequest, GetWorkTasksQuery>();
         }
     }
 }
